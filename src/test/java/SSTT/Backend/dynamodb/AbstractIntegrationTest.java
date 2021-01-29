4893d43bb66b7f3a06cd1c76a41a943c178128e4
Kim-SuBin <sb2504@naver.com>
2021-01-29 20:59:52 +0900

    [Add] ✨ DynamoDb - testcontainers 적용

---
+++ src/test/java/SSTT/Backend/dynamodb/AbstractIntegrationTest.java
@@ -0,0 +1,29 @@
+package SSTT.Backend.dynamodb;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers(disabledWithoutDocker = true)
+public abstract class AbstractIntegrationTest {
+
+    private static final GenericContainer dynamodb =
+            new GenericContainer("amazon/dynamodb-local:latest").withExposedPorts(8000);
+
+    static {
+        dynamodb.start();
+    }
+
+    @DynamicPropertySource
+    static void dynamoDbProperties(DynamicPropertyRegistry registry) {
+        final String endpoint = String.format("http://%s:%s",
+                dynamodb.getContainerIpAddress(),
+                dynamodb.getMappedPort(8000));
+        registry.add("aws.dynamo.endpoint", () -> endpoint);
+    }
+
+
+}
