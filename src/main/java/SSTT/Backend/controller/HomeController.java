3996eb3fde21c2366299ce697e8dc50136356432
Kim-SuBin <sb2504@naver.com>
2021-01-30 19:23:22 +0900

    [Update] ✍ Rename

--- src/main/java/SSTT/Backend/controller/HomeController.java
+++ src/main/java/SSTT/Backend/controller/HomeController.java
@@ -1,13 +1,15 @@
 package SSTT.Backend.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
     @GetMapping("")
-    public String home() {
-        return "home";
+    public String mainpage() {
+        return "mainpage";
     }
 }
