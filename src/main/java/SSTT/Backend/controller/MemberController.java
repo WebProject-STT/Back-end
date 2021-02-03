84a7e369e364f6f9fb73915ac039d936da69d36b
Kim-SuBin <sb2504@naver.com>
2021-02-04 06:15:11 +0900

    [Add] ✨ Member Controller - signup, login

---
+++ src/main/java/SSTT/Backend/controller/MemberController.java
@@ -0,0 +1,39 @@
+package SSTT.Backend.controller;
+
+import SSTT.Backend.dto.MemberDto;
+import SSTT.Backend.service.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class MemberController {
+    private MemberService memberService;
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("member", new MemberDto());
+
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(MemberDto memberDto) {
+        memberService.signUp(memberDto);
+
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
