df9a202e9373a346ba34970cb5e0ce678fc1dc10
Kim-SuBin <sb2504@naver.com>
2021-02-07 11:28:04 +0900

    [Update] 🎨 Member DTO
    사용하지 않는 import 제거

--- src/main/java/SSTT/Backend/dto/MemberDto.java
+++ src/main/java/SSTT/Backend/dto/MemberDto.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
