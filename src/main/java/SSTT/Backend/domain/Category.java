f2b59b17ad2db373b8d88a07706789c3f92a11f1
Kim-SuBin <sb2504@naver.com>
2021-02-11 06:01:15 +0900

    [Refact] ♻ Category
    회원에 관계없이 카테고리가 동일하게 생성되므로 양방향에서 단방향으로 수정

--- src/main/java/SSTT/Backend/domain/Category.java
+++ src/main/java/SSTT/Backend/domain/Category.java
@@ -1,23 +1,41 @@
 package SSTT.Backend.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cg_id")
-    private Long cg_id; // 카테고리 번호
+    private Long id; // 카테고리 번호
+
+    // 회원에 관계 없이 모든 카테고리는 동일하게 생성됨
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member; // 회원 번호
 
     @Column(name = "cg_title")
     private String title; // 카테고리명
 
     @OneToMany(mappedBy = "category")
     private List<Contents> contentsList = new ArrayList<>(); // 게시글 리스트
+
+    // 연관관계 편의 메소드
+//    public void setMember(Member member) {
+//        this.member = member;
+//        member.getCategoryList().add(this);
+//    }
+
+    @Builder
+    public Category(Long id, String title, List<Contents> contentsList) {
+        this.id = id;
+        this.title = title;
+        this.contentsList = contentsList;
+    }
 }
