00d47b9aa0be63022eb872810edea50083924717
Kim-SuBin <sb2504@naver.com>
2021-02-11 06:06:18 +0900

    [Update] ✨ 회원가입, 로그인

--- src/main/java/SSTT/Backend/controller/MemberController.java
+++ src/main/java/SSTT/Backend/controller/MemberController.java
@@ -1,40 +1,64 @@
 package SSTT.Backend.controller;
 
-import SSTT.Backend.dto.MemberDto;
+import SSTT.Backend.config.security.JwtTokenProvider;
+import SSTT.Backend.domain.Member;
+import SSTT.Backend.dto.MemberLoginDto;
+import SSTT.Backend.dto.MemberSignUpDto;
+import SSTT.Backend.repository.MemberRepository;
 import SSTT.Backend.service.MemberService;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@AllArgsConstructor
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
-    private MemberService memberService;
 
-    @GetMapping("/")
-    public String index() {
-        return "home";
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+
+    @ApiOperation(value = "회원 가입", notes = "회원 번호 반환. 아이디 중복 시 -1 (long) 반환")
+    @PostMapping("/user/signup")
+    public Long signup(@RequestBody @Valid MemberSignUpDto member) {
+        try {
+            memberService.validateDuplicateMember(member.getSignId());
+        } catch (IllegalArgumentException e) {
+            System.out.println("아이디 중복 오류");
+            long result = -1;
+            return result;
+        }
+        return memberRepository.save(
+                Member.builder()
+                        .signId(member.getSignId())
+                        .name(member.getName())
+                        .pwd(passwordEncoder.encode(member.getPwd()))
+                        .email(member.getEmail())
+                        .roles(Collections.singletonList("USER")) // 최초 가입 시 USER로 설정
+                        .signupDt(LocalDateTime.now())
+                        .build()).getId();
     }
 
-    @GetMapping("/signup")
-    public String signup(Model model) {
-        model.addAttribute("member", new MemberDto());
+    @ApiOperation(value = "로그인", notes = "토큰 반환. 아이디 또는 패스워드 오류 시 false (string) 반환")
+    @PostMapping("/user/login")
+    public String login(@RequestBody @Valid MemberLoginDto user) {
+        Member member;
+        try {
+            member = memberService.findSignId(user.getSignId());
+            if (!passwordEncoder.matches(user.getPwd(), member.getPassword())) {
+                throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            return "false";
+        }
 
-        return "signup";
-    }
-
-    @PostMapping("/signup")
-    public String signup(MemberDto memberDto) {
-        memberService.idCheck(memberDto.getSignId()); // ID 중복 확인
-        memberService.signUp(memberDto);
-
-        return "home";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
     }
 }
--- src/main/java/SSTT/Backend/service/MemberService.java
+++ src/main/java/SSTT/Backend/service/MemberService.java
@@ -1,88 +1,40 @@
 package SSTT.Backend.service;
 
 import SSTT.Backend.domain.Member;
-import SSTT.Backend.domain.Role;
-import SSTT.Backend.dto.MemberDto;
 import SSTT.Backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
-@Transactional
 @RequiredArgsConstructor // final 선언과 관련된 lombok 기능
 public class MemberService implements UserDetailsService{
 
     private final MemberRepository memberRepository;
 
-    // 회원가입
-    @Transactional
-    public Long signUp(MemberDto memberDto) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        memberDto.setPwd(passwordEncoder.encode(memberDto.getPwd()));
-        memberDto.setSignupDt(LocalDateTime.now());
-
-        return memberRepository.save(memberDto.toEntity()).getId();
-    }
-
-    public void idCheck(String signId) {
-        Optional<Member> findMembers = memberRepository.findBySignId(signId);
-        if (!findMembers.isEmpty()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+    // 아이디 중복 조회
+    public void validateDuplicateMember(String signId) {
+        Optional<Member> findMember = memberRepository.findBySignId(signId);
+        if (!findMember.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
         }
     }
 
+    // 회원 정보 조회를 signId 이용
     @Override
-    public UserDetails loadUserByUsername(String signId) throws UsernameNotFoundException {
-        // 로그인을 하기 위해 가입된 user 정보 조회
-        Optional<Member> memberWrapper = memberRepository.findBySignId(signId);
-        Member member = memberWrapper.get();
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-
-        // (간단하게!!) signId이 admin이면 admin 권한 부여. 아니면 member 권한 부여
-        if("admin".equals(signId)){
-            authorities.add(new SimpleGrantedAuthority(Role.ADMIN.getValue()));
-        } else {
-            authorities.add(new SimpleGrantedAuthority((Role.MEMBER.getValue())));
-        }
-
-        // 아이디, 비밀번호, 권한리스트를 매개변수로 User를 만들어 반환
-        return new User(member.getSignId(), member.getPwd(), authorities);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findBySignId(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
     }
-    
-//    // 회원 가입
-//    public Long join(Member member){
-//
-//        validateDuplicateUser(member); // 중복 회원 검증
-//
-//        memberRepository.save(member);
-//        return member.getId();
-//    }
-//
-//    // 중복 회원 검증
-//    private void validateDuplicateUser(Member member) {
-//        // 문제 발생 시 EXCEPTION 발생
-//        List<Member> findMembers = memberRepository.findBySignId(member.getSignId());
-//        if (!findMembers.isEmpty()) {
-//            throw new IllegalStateException("이미 존재하는 회원입니다.");
-//        }
-//    }
-//
-//    public Member findOne(Long memberId){
-//        return memberRepository.findOne(memberId);
-//    }
+
+    // 회원 아이디 조회
+    public Member findSignId(String signId){
+        return memberRepository.findBySignId(signId)
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 ID 입니다."));
+    }
 
 }
