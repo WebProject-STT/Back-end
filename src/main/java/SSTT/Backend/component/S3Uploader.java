924ffd1d09af512f9c6b2d52d43a662e0bc4f294
Kim-SuBin <sb2504@naver.com>
2021-02-16 07:38:16 +0900

    [Update] ✨ Unique File Name

--- src/main/java/SSTT/Backend/component/S3Uploader.java
+++ src/main/java/SSTT/Backend/component/S3Uploader.java
@@ -1,8 +1,11 @@
 package SSTT.Backend.component;
 
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Slf4j
@@ -25,6 +29,7 @@ public class S3Uploader {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    // MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
         File uploadFile = convert(multipartFile)
                 .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
@@ -33,14 +38,14 @@ public class S3Uploader {
     }
 
     private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + uploadFile.getName();
-        String uploadImageUrl = putS3(uploadFile, fileName);
-        removeNewFile(uploadFile);
-        return uploadImageUrl;
+        String fileName = dirName + "/" + LocalDateTime.now() + uploadFile.getName();
+        String uploadUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile); // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
+        return uploadUrl; // 업로드된 파일의 S3 URL 주소 반환
     }
 
     private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead)); // PublicRead 권한으로 업로드 됨
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
@@ -63,57 +68,5 @@ public class S3Uploader {
 
         return Optional.empty();
     }
+
 }
-
-
-//@Slf4j
-//@RequiredArgsConstructor
-//@Component
-//public class S3Uploader {
-//
-//    private final static String TEMP_FILE_PATH = "src/main/resources/";
-//
-//    private final AmazonS3Client amazonS3Client;
-//
-//    @Value("${cloud.aws.s3.bucket}")  // 프로퍼티에서 cloude.aws.s3.bucket에 대한 정보를 불러옵니다.
-//    public String bucket;
-//
-//    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-//        File convertedFile = convert(multipartFile);
-//        return upload(convertedFile, dirName);
-//    }
-//
-//    long cnt = 1;
-//    private String upload(File uploadFile, String dirName) {
-//        String fileName = dirName + "/" + String.valueOf(cnt) + uploadFile.getName();
-//        String uploadUrl = putS3(uploadFile, fileName);
-//        removeNewFile(uploadFile);
-//        return uploadUrl;
-//    }
-//
-//    private String putS3(File uploadFile, String fileName) {
-//        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
-//        return amazonS3Client.getUrl(bucket, fileName).toString();
-//    }
-//
-//    private void removeNewFile(File targetFile) {
-//        if (targetFile.delete()) {
-//            return;
-//        }
-//        log.info("임시 파일이 삭제 되지 못했습니다. 파일 이름: {}", targetFile.getName());
-//    }
-//
-//    private File convert(MultipartFile file) throws IOException {
-//        log.info(TEMP_FILE_PATH);
-//        File convertFile = new File(TEMP_FILE_PATH + file.getOriginalFilename());
-//        if (convertFile.createNewFile()) {
-//            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-//                fos.write(file.getBytes());
-//                log.info("convert 통과");
-//            }
-//            return convertFile;
-//        }
-//        throw new IllegalArgumentException(String.format("파일 변환이 실패했습니다. 파일 이름: %s", file.getName()));
-//    }
-//
-//}
