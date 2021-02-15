9028fb1df25ded0e726ed52b4841e539fd4e14ec
Kim-SuBin <sb2504@naver.com>
2021-02-16 02:01:29 +0900

    [Update] ✨ Change Uploader Code

--- src/main/java/SSTT/Backend/component/S3Uploader.java
+++ src/main/java/SSTT/Backend/component/S3Uploader.java
@@ -1,5 +1,6 @@
 package SSTT.Backend.component;
 
+
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
@@ -12,30 +13,30 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class S3Uploader {
 
-    private final static String TEMP_FILE_PATH = "src/main/resources/";
-
     private final AmazonS3Client amazonS3Client;
 
-    @Value("${cloud.aws.s3.bucket}")  // 프로퍼티에서 cloude.aws.s3.bucket에 대한 정보를 불러옵니다.
-    public String bucket;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File convertedFile = convert(multipartFile);
-        return upload(convertedFile, dirName);
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
+
+        return upload(uploadFile, dirName);
     }
 
-    long cnt = 1;
     private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + String.valueOf(cnt) + uploadFile.getName();
-        String uploadUrl = putS3(uploadFile, fileName);
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
         removeNewFile(uploadFile);
-        return uploadUrl;
+        return uploadImageUrl;
     }
 
     private String putS3(File uploadFile, String fileName) {
@@ -45,23 +46,74 @@ public class S3Uploader {
 
     private void removeNewFile(File targetFile) {
         if (targetFile.delete()) {
-            return;
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
         }
-        log.info("임시 파일이 삭제 되지 못했습니다. 파일 이름: {}", targetFile.getName());
     }
 
-    private File convert(MultipartFile file) throws IOException {
-        File convertFile = new File(TEMP_FILE_PATH + file.getOriginalFilename());
-        if (convertFile.createNewFile()) {
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if(convertFile.createNewFile()) {
             try (FileOutputStream fos = new FileOutputStream(convertFile)) {
                 fos.write(file.getBytes());
-                log.info("==========================");
-                log.info("convert 통과");
-                log.info("==========================");
             }
-            return convertFile;
+            return Optional.of(convertFile);
         }
-        throw new IllegalArgumentException(String.format("파일 변환이 실패했습니다. 파일 이름: %s", file.getName()));
-    }
 
+        return Optional.empty();
+    }
 }
+
+
+//@Slf4j
+//@RequiredArgsConstructor
+//@Component
+//public class S3Uploader {
+//
+//    private final static String TEMP_FILE_PATH = "src/main/resources/";
+//
+//    private final AmazonS3Client amazonS3Client;
+//
+//    @Value("${cloud.aws.s3.bucket}")  // 프로퍼티에서 cloude.aws.s3.bucket에 대한 정보를 불러옵니다.
+//    public String bucket;
+//
+//    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+//        File convertedFile = convert(multipartFile);
+//        return upload(convertedFile, dirName);
+//    }
+//
+//    long cnt = 1;
+//    private String upload(File uploadFile, String dirName) {
+//        String fileName = dirName + "/" + String.valueOf(cnt) + uploadFile.getName();
+//        String uploadUrl = putS3(uploadFile, fileName);
+//        removeNewFile(uploadFile);
+//        return uploadUrl;
+//    }
+//
+//    private String putS3(File uploadFile, String fileName) {
+//        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+//        return amazonS3Client.getUrl(bucket, fileName).toString();
+//    }
+//
+//    private void removeNewFile(File targetFile) {
+//        if (targetFile.delete()) {
+//            return;
+//        }
+//        log.info("임시 파일이 삭제 되지 못했습니다. 파일 이름: {}", targetFile.getName());
+//    }
+//
+//    private File convert(MultipartFile file) throws IOException {
+//        log.info(TEMP_FILE_PATH);
+//        File convertFile = new File(TEMP_FILE_PATH + file.getOriginalFilename());
+//        if (convertFile.createNewFile()) {
+//            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+//                fos.write(file.getBytes());
+//                log.info("convert 통과");
+//            }
+//            return convertFile;
+//        }
+//        throw new IllegalArgumentException(String.format("파일 변환이 실패했습니다. 파일 이름: %s", file.getName()));
+//    }
+//
+//}
