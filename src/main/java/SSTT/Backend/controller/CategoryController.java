f290ef06a49a0933ebec126d0f1e037eec48bc7f
Kim-SuBin <sb2504@naver.com>
2021-02-15 23:21:59 +0900

    [Refact] ♻ Category Controller
    카테고리 삭제  부분 Post에서 Delete로 변경

--- src/main/java/SSTT/Backend/controller/CategoryController.java
+++ src/main/java/SSTT/Backend/controller/CategoryController.java
@@ -6,10 +6,7 @@ import SSTT.Backend.service.CategoryService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,20 +18,20 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @ApiOperation(value = "카테고리 생성", notes = "카테고리 번호 반환")
-    @PostMapping("/category/create")
+    @PostMapping("/category")
     public Long create(@RequestBody @Valid CategoryDto categoryDto) {
         return categoryService.create(categoryDto);
     }
 
     @ApiOperation(value = "카테고리 삭제", notes = "카테고리 삭제 성공 시 true 반환")
-    @PostMapping("/category/delete")
-    public Boolean delete(@RequestBody @Valid CategoryDto categoryDto) {
-        return categoryService.delete(categoryDto.getId());
+    @DeleteMapping("/category/{categoryId}")
+    public Boolean delete(@PathVariable("categoryId") Long id) {
+        return categoryService.delete(id);
     }
 
     @ApiOperation(value = "카테고리 목록 조회", notes = "카테고리 리스트 전달")
     @GetMapping("/category")
-    public List<Category> findAll(Model model) {
+    public List<Category> findAll() {
         return categoryService.findAll();
     }
 
