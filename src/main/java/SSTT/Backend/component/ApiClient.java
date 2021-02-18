6fd99810db2efda9de70ef353775db90246397ed
Kim-SuBin <sb2504@naver.com>
2021-02-18 23:20:55 +0900

    [Update] ✨ API Client
    포트 번호 변경

--- src/main/java/SSTT/Backend/component/ApiClient.java
+++ src/main/java/SSTT/Backend/component/ApiClient.java
@@ -23,7 +23,7 @@ public class ApiClient {
 
     public Map<String, Object> getApi(String filename, String extension, String s3url, Integer subjectNum) {
 
-        String url = "http://localhost:8081/test";
+        String url = "http://localhost:8080/test";
 
 
         List<String> fileInfo = new ArrayList<>();
