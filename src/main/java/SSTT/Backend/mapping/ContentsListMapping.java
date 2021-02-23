41b5e3706a8d9a2294d6b3803a9e5f4651339312
Kim-SuBin <sb2504@naver.com>
2021-02-24 00:45:13 +0900

    [Update] 🛢 add category

--- src/main/java/SSTT/Backend/mapping/ContentsListMapping.java
+++ src/main/java/SSTT/Backend/mapping/ContentsListMapping.java
@@ -1,9 +1,12 @@
 package SSTT.Backend.mapping;
 
+import SSTT.Backend.domain.Category;
+
 import java.time.LocalDateTime;
 
 public interface ContentsListMapping {
     Long getId();
     String getTitle();
     LocalDateTime getDate();
+    Category getCategory();
 }
